323a248a1f1c168dd0ec9445a10bb5d24939b0f0
Abdallah Ibrahim <abdallahelhdad223@gmail.com>
2026-07-18 04:43:25 +0300

    Fix: sync AutoSubscribe column migration for Lectures table

---
+++ Migrations/20260718013930_SyncLectureAutoSubscribeColumn.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncLectureAutoSubscribeColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                ALTER TABLE ""Lectures"" 
+                ADD COLUMN IF NOT EXISTS ""AutoSubscribe"" boolean NOT NULL DEFAULT false;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AutoSubscribe",
+                table: "Lectures");
+        }
+    }
+}
